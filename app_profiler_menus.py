80611a446c6965b994b02bdd549e09697c54ae5c
kuts01 <kutloanosikosana@gmail.com>
2026-01-28 14:38:13 +0200

    Update app_profiler_menus.py

--- app_profiler_menus.py
+++ app_profiler_menus.py
@@ -131,5 +131,6 @@ elif menu == "STEM Data Explorer":
 elif menu == "Contact":
     # Add a contact section
     st.header("Contact Information")
-    email = "jane.doe@example.com"
+    email = "kutloanosikosana@gmail.com"
+
     st.write(f"You can reach me at {email}.")
